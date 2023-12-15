d3005043fca6dd3f65b7d592ba44019bfc382376
bilalmuhammad41 <bilalmohammad41@gmail.com>
2023-12-15 15:42:09 +0500

    Started working on Project section - Rendered project titles.

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {NavItems} from '../../constants'
 
 const Projects = () => {
   return (
-    <section className='bg-black xl:max-w-[1700px]'>
-      <h2 className=' text-xl'>
-        Projects
-      </h2>
+    <section className='w-full md:h-[100vh] sm:[] bg-black xl:max-w-[1700px]'>
+      <div>
+        {NavItems.filter(item=> !['playground', 'contact'].includes(item)).map((item)=>(
+          <div className='h-[100px] border-white border-b-[1px]'>
+            <h1 className='text-white text-[80px] uppercase font-semibold'>{item}</h1>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
