b0b272a4f8ab235dc7873ede5c9bf858056a2d5f
bilalmuhammad41 <bilalmohammad41@gmail.com>
2023-12-26 00:48:06 +0500

    Footer fixes

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = ({formattedTime}) => {
 
 
   return (
-    <footer className='' id='footer'>
-      <div className='flex flex-col-reverse gap-5 ss:flex-row  justify-between w-[100svw] px-5 pb-4 md:pt-[15svh] sm:pt-[10svh] ss:pt-[10svh] xs:pt-[7svh] text-[0.9rem]'>
-        <div className='text-white font-semibold'>2023 &nbsp;&nbsp;&nbsp; {formattedTime} PST</div>
+    <footer className='w-full px-5' id='footer'>
+      <div className='flex flex-col-reverse gap-4 ss:flex-row justify-between m-auto pb-4 md:pt-[15svh] sm:pt-[10svh] ss:pt-[10svh] xs:pt-[7svh] text-[0.9rem]'>
+        <div className='text-white font-semibold '>2023 &nbsp;&nbsp;&nbsp; {formattedTime} PST</div>
         
         <ul className='flex '>{socials.map((social, index)=>(
-          <li className='list-none mr-5 uppercase font-semibold text-white underline-effect' key={index}>
+          <li className={`list-none sm:${index < socials.length-1 ? 'mr-5' :''} ${index < socials.length-1 ? 'mr-4' :''}  uppercase font-semibold text-white underline-effect`} key={index}>
             <a href={social.link}>{social.name}</a>
           </li>
         ))}
